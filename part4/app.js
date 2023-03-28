7b43942464b1ef2cbda5595789345b3550316a2c
Juanjo Simon <jjotasaimon@gmail.com>
2023-03-28 23:03:08 +0700

    Exercises 4.1 - 4.2 ready

--- part4/app.js
+++ part4/app.js
@@ -3,15 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const { Blog } = require("./models/blog");
+const blogsRouter = require("./controllers/blog");
 
 const password = process.env.PASSWORD;
 
@@ -23,19 +16,6 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
