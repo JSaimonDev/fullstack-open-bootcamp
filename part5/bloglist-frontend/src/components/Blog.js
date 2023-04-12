ddbec843989f82376935b84ea47c66086f3685b1
Juanjo Simon <114825096+DiscoStuck@users.noreply.github.com>
2023-04-12 15:22:29 +0700

    Revert "Exercises 5.5 to 5.10 ready"
    This reverts commit 70d0f7494b4ccccef0c2dda2a9b7c734d315cce3.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,43 +1,7 @@
-import Toggleable from "./toggleable"
-import {updateBlog, deleteBlog } from "../services/blogs"
-
-
-
-const Blog = ({blog, setBlogs, blogs}) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-
-  const handleLike = async (blog, blogs) => {
-    const newBlog = {...blog, likes: blog.likes + 1}
-    const blogIsUpdated = await updateBlog(newBlog.id, newBlog)
-    if (blogIsUpdated){
-      const updatedBlogs = blogs.map((blog) => blog.id === blogIsUpdated.id ? blogIsUpdated : blog)
-      setBlogs(updatedBlogs)
-    } 
-  }
-
-  const handleDelete = async (blog) => {
-    const deleteStatus = await deleteBlog(blog.id)
-    if (deleteStatus === 204){
-      const updatedBlogs = blogs.filter((b) => b.id !== blog.id)
-      setBlogs(updatedBlogs)
-    }
-    else {
-      console.log('error deleting blog')
-    }
-  }
-
-  
-
-  return (
-  <div style={blogStyle}>
-    <Toggleable buttonShow={'Show blog'} buttonHide={'Hide blog'}>{blog.title} &nbsp; <button onClick={() => handleDelete(blog)}>Delete</button><br />{blog.url}<br />{blog.likes}<button onClick={()=>handleLike(blog, blogs)}>like</button><br />{blog.author}<br /></Toggleable>
-  </div>  )
-}
+const Blog = ({blog}) => (
+  <div>
+    {blog.title} {blog.author}
+  </div>  
+)
 
 export default Blog
